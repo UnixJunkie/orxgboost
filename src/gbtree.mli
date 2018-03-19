8cdbaad77dde56078e6e53725afd6c62563c4a86
Francois BERENGER <ligand@free.fr>
2018-03-19 10:09:29 +0900

    added some ocamldoc interested users will have to look into the R documentation of xgboost though

--- src/gbtree.mli
+++ src/gbtree.mli
@@ -28,9 +28,32 @@ type nb_columns = int
 type sparsity = Dense
               | Sparse of nb_columns
 
+(** [train sparsity nrounds params data_fn labels_fn]
+    will train a gradient-boosted tree using given parameters
+    on the data in [data_fn] with labels in [labels_fn].
+    [data_fn] is a numerical matrix dumped in a tab-separated text file
+    without any format header.
+    Dense or sparse matrices (in CSR format) are supported.
+    Rows are observations, columns are features.
+    [labels_fn] is a vector of tab-separated "1" or "-1" integer labels
+    in a text file, without any format header.
+    Column [i] in [labels_fn] is the corresponding label of line [i]
+    in [data_fn]. *)
 val train: ?debug:bool ->
   sparsity -> int -> booster -> filename -> filename -> Result.t
 
+(** [predict sparsity model data_fn] will run the previously trained
+    gradient-boosted tree on the data stored in [data_fn].
+    [data_fn] must use the same format than the file that was used
+    during training.
+    On success, a filename is returned.
+    This text file contains the predicted decision values,
+    one per line of [data_fn]. *)
 val predict: ?debug:bool -> sparsity -> Result.t -> filename -> Result.t
 
+(** [read_predictions ?debug result] will decode predicted decision values
+    in [result], or crash if the previous call to [predict]
+    was not successful.
+    Upon success and if [not debug], the file containing the
+    predicted decision values is removed. *)
 val read_predictions: ?debug:bool -> Result.t -> float list
